526dc3b8cf973c9f4df795747449d17ac6260361
Micah Nolte <holler@micahnolte.com>
2013-10-20 15:01:13 -0500

    First version.

---
+++ node/run.js
@@ -0,0 +1,49 @@
+var socketIo        = require('socket.io'),
+	twitter         = require('ntwitter'),
+	// Fill these in with your own bidness.
+	twitAuth        = {
+		consumer_key: '',
+		consumer_secret: '',
+		access_token_key: '',
+		access_token_secret: ''
+	},
+	io,
+	t,
+	tweets = [];
+
+
+init();
+
+// =========================================================
+
+function init() {
+	socketInit();
+	twitterInit();
+}
+
+
+// ========================== Socket.io ==========================
+
+function socketInit() {
+	io = socketIo.listen(8080);
+	io.sockets.on('connection', socketConnect);
+}
+
+function socketConnect(socket) {
+	socket.emit('tweets', tweets);
+}
+
+
+// ========================== Twitter Stream ==========================
+
+function twitterInit() {
+	t = new twitter( twitAuth );
+	t.stream('statuses/filter', { track: 'ridingthel', follow: '54906888' }, twitterConnect );
+}
+
+function twitterConnect(stream) {
+	stream.on('data', function(tweet) {
+		tweets.push( tweet );
+		io.sockets.emit( 'tweets', [tweet] );
+	});
+}
